937cc641d0a0081593d96ec030cfd07d203aa867
Dhammpal Dhundle <dhammpal2812@gmail.com>
2025-07-24 17:15:25 +0530

    add business modal

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
-import './index.css'
-import App from './App.tsx'
-import '../../OurCity/src/style/custom.css'
+  
+import './index.css';
+import App from './App.tsx';
+import "../../OurCity-Main/src/style/custom.css";
 
 createRoot(document.getElementById('root')!).render(
-
   <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-    <App />
-  </PersistGate>
-</Provider>
-
-)
+    <PersistGate loading={null} persistor={persistor}>
+      
+        <App />
+      
+    </PersistGate>
+  </Provider>
+);
